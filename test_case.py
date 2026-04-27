24d63577a58228745c902a39f64530b4cc02e486
zhibinhuosheng <135718918+zhibinhuosheng@users.noreply.github.com>
2026-04-27 15:32:54 +0800

    Add initial test case structure in test_case.py

---
+++ test_case.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+class Test_XXX:
+    """
+    @TestCaseNumber:
+    @TestCaseName:
+    @PreCondition:
+        1、
+        2、
+    @TestStep:
+        1.
+        2.
+    @ExpectedResult:
+        1.
+        2.
+    @Return:
+    @ModifyRecord:
+    """
+
+    def Precondition(self):
+        pass
+
+    def test_Procedure(self):
+        pass
+
+    def Postcondition(self):
+        pass
